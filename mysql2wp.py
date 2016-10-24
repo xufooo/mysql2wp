2c66e15ebc7ad8088cb97c89f8c37579cdfbbc56
OoO <xufooo@gmail.com>
2016-10-24 16:46:35 +0800

    add post datetime todo: add log to make sure no duplication

--- mysql2wp.py
+++ mysql2wp.py
@@ -1,5 +1,6 @@
 # -*-coding:utf-8 -*-
 
+from datetime import datetime
 from wordpress_xmlrpc import Client, WordPressPost
 from wordpress_xmlrpc.methods.posts import GetPosts,NewPost
 
@@ -7,11 +8,13 @@ from wordpress_xmlrpc.methods.posts import GetPosts,NewPost
 wp = Client('http://xxxx/xmlrpc.php', 'xxxx', 'xxxx')
 post = WordPressPost()
 post.title = 'Test Title'
+post.date = datetime.strptime('xxxx-xx-xx xx:xx:xx', '%Y-%m-%d %H:%M:%S')
 post.terms_names = {
-	'category': ['Test Category'],
+	'category': ['Uncategorized'],
 }
 post.content = '<p>Test Contents' + \
-			   '</p><img class="alignnone size-medium" src="xxx"' +\
+			   '</p><img src="xxx"' +\
 			   ' />'
+post.post_status = 'publish'
 
 wp.call(NewPost(post))
